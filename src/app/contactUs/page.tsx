a7efbc3293e2bf3ef74cecabd8c376ceccc201be
ViacheslavDotsenko <nab.vyacheslav.dotsenko@gmail.com>
2024-11-21 17:50:11 +0200

    initial

---
+++ src/app/contactUs/page.tsx
@@ -0,0 +1,9 @@
+
+
+export default function ContactUs() {
+    return (
+        <><h1>hellow</h1>
+        </>
+    )
+    
+}
---
+++ src/app/not-found.tsx
@@ -0,0 +1,5 @@
+
+export default function error() {
+    return(<h1>ERROR Page not found</h1>)
+    
+}
