59e7f645371eb2907947595f62498851ea1a85ab
ViacheslavDotsenko <nab.vyacheslav.dotsenko@gmail.com>
2024-11-26 11:04:22 +0200

    added menu

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -8,6 +8,8 @@ import IconBurger from "../socialIcon/IconBurger";
 const Header: React.FC = () => {
   
   const [isMobile, setIsMobile] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isShowBurger, setIsShowBurger] = useState(true)
 
  
   useEffect(() => {
@@ -19,18 +21,45 @@ const Header: React.FC = () => {
     return () => window.removeEventListener("resize", handleResize);
   }, []);
 
+  useEffect(() => {
+    
+    if (isMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = 'auto';
+    }
+  }, [isMenuOpen]);
+  
+  const openMenu = () => {
+    setIsMenuOpen(true);    
+    setIsShowBurger(false)    
+  };
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+    setIsShowBurger(true)
+  }
+
+
+
   return (
     <header className="w-full flex justify-center p-[1rem]">
       <div className="flex max-w-screen-xl w-full justify-between">
         <Link href="/">Logo Here</Link>
-        {!isMobile && <Nav />}
-        
-        <div>
-          
+        {!isMobile && <Nav />}        
+        <div className="cursor-pointer" onClick={openMenu}>          
           {!isMobile && <ActivIcon isBlackLog={false} isBlackBask={false} />}
-          {isMobile && <IconBurger isShow={true} />}
+          {isMobile && <IconBurger isShow={isShowBurger} />}
         </div>
       </div>
+      {isMenuOpen && <div className="w-[100vw] h-[100vh} bg-[black] text-white p-[1rem]">
+        <div className="flex w-full justify-between mb-[1rem]">
+          <Link href="/">Logo Here</Link>
+          <span onClick={closeMenu} className="w-[24px] h-[24px] relative before:content-[''] after:content-[''] before:absolute after:absolute before:top-1/2 after:top-1/2 before:left-1/2 after:left-1/2 before:w-full after:w-full before:h-[2px] after:h-[2px] before:bg-white after:bg-white before:-translate-x-1/2 after:-translate-x-1/2 before:-translate-y-1/2 after:-translate-y-1/2 before:rotate-45 after:-rotate-45 cursor-pointer"></span>
+          </div> 
+          <Nav horizontal={false} />
+          <div className="mt-[1rem] flex w-full justify-center"><ActivIcon isBlackLog={true} isBlackBask={true} /></div>
+          </div>}
+      
     </header>
   );
 };
--- src/components/navigation/NavList.tsx
+++ src/components/navigation/NavList.tsx
@@ -1,20 +1,32 @@
 import React from "react";
+import Link from "next/link";
 
-
-
-const Nav = () => {
-    return(
-        <nav className="flex">
-          <ul className="flex flex-row">
-            <li className="mr-[6%]">Home</li>
-            <li className="mr-[6%]">Features</li>
-            <li className="mr-[6%]">Blog</li>
-            <li className="mr-[6%]">Shop</li>
-            <li className="mr-[6%]">About</li>
-            <li className="">Contact</li>
-          </ul>
-        </nav>
-    )
+interface NavListProps {
+  horizontal?: boolean;
 }
 
-export default Nav;
+const navItems = [
+  { label: "Home", url: "/" },
+  { label: "Features", url: "/features" },
+  { label: "Blog", url: "/blog" },
+  { label: "Shop", url: "/shop" },
+  { label: "About", url: "/about" },
+  { label: "Contact", url: "/contact" }
+];
+
+const NavList: React.FC<NavListProps> = ({ horizontal = true }) => {
+  return (
+    <nav className="flex">
+      <ul className={`flex ${horizontal ? 'flex-row' : 'flex-col'}`}>
+        {navItems.map((item, index) => (          
+          <li key={index} className={index === navItems.length - 1 ? "" : "mr-[6%]"}>
+            <Link href={item.url}>{item.label}</Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default NavList;
+
